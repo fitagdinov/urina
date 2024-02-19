ec03c16260ca9067a77d28ba2a12b98d84a4b772
Robert <robertfitagdinov@gmail.com>
2024-02-20 01:25:41 +0300

    add detection

--- detection/detection_main.py
+++ detection/detection_main.py
@@ -1,29 +1,27 @@
 from ultralytics import YOLO
 import cv2
 import matplotlib.pyplot as plt
-model = YOLO("best.pt")
-img = cv2.imread('../data/not_used(test)/20240116_002642.jpg')
-results = model.predict(img, stream=True)
-for result in results:
-    boxes = result.boxes.cpu().numpy()  # Get boxes on CPU in numpy format
-    print(len(boxes))
-    for box in boxes:  # Iterate over boxes
-        r = box.xyxy[0].astype(int)  # Get corner points as int
-        class_id = int(box.cls[0])  # Get class ID
-        class_name = model.names[class_id]  # Get class name using the class ID
-        print(f"Class: {class_name}, Box: {r}")  # Print class name and box coordinates
+def model_init(path= "best.pt"):
+    model = YOLO(path)
+    return model
+def result(model,img):
+    results = model.predict(img, stream=True)
+    boxes=[]
+    for result in results:
+        boxes += result.boxes.cpu().numpy()  # Get boxes on CPU in numpy format
+    return [box.xyxy[0].astype(int) for box in boxes]
+def show(model,img):
+    boxes = result(model,img)
+    for r in boxes:
         cv2.rectangle(img, r[:2], r[2:], (0, 255, 0), 2)  # Draw boxes on the image
-# img = cv2.resize(img,(680,320))
-scale_percent = 20  # percent of original size
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-dim = (width, height)
-print(dim)
-dim=(640, 480)
-# resize image
-img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-print(img.shape)
-cv2.imshow('im', img)
-# plt.imshow(img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+
+    dim = (640, 480)
+    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    print(img.shape)
+    cv2.imshow('The best image in the world/ URINA', img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+if __name__ == '__main__':
+    img = cv2.imread('../data/not_used(test)/20240116_002642.jpg')
+    model = model_init(path= "best.pt")
+    show(model, img)
